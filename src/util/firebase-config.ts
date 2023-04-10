673f6ffe2c94a2272364b72bef7d55af49142fdd
Vedant Dhote <85186983+Vedant-code@users.noreply.github.com>
2023-04-10 11:41:31 +0530

    Update firebase-config.ts

--- src/util/firebase-config.ts
+++ src/util/firebase-config.ts
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA1b8UTyciPBN38UgLXCO7TFV6PeTayHGA",
-  authDomain: "rocket-share-41e79.firebaseapp.com",
-  projectId: "rocket-share-41e79",
-  storageBucket: "rocket-share-41e79.appspot.com",
-  messagingSenderId: "946968656086",
-  appId: "1:946968656086:web:18aaf25c4c47378c173177"
+  apiKey: "AIzaSyDy2pFde6l_2LbthE2vWUvJIRzGqucsFmY",
+  authDomain: "lightning-share-45c22.firebaseapp.com",
+  projectId: "lightning-share-45c22",
+  storageBucket: "lightning-share-45c22.appspot.com",
+  messagingSenderId: "1086912195503",
+  appId: "1:1086912195503:web:448a8ac24cf6c994b05d5a",
+  measurementId: "G-T91VFK74Z2"
 };
 
 export default firebaseConfig;
