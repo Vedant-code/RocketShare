7e81238bee62dd0dc410ca623f237aebccba4fde
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2023-04-10 11:33:14 +0530

    Create firebase-config.ts

---
+++ src/util/firebase-config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1b8UTyciPBN38UgLXCO7TFV6PeTayHGA",
+  authDomain: "rocket-share-41e79.firebaseapp.com",
+  projectId: "rocket-share-41e79",
+  storageBucket: "rocket-share-41e79.appspot.com",
+  messagingSenderId: "946968656086",
+  appId: "1:946968656086:web:18aaf25c4c47378c173177"
+};
+
+export default firebaseConfig;
